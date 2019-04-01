5a53f071830a2f4cd7c7fa095c1fc133a94db948
Sascha Selzer <sascha.selzer@innoq.com>
2019-04-01 21:50:21 +0200

    Add Dockerfile and docker-compose to quarkus-demo

---
+++ graalvm/quarkus-demo/db-init/V2_example_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO todos (content) values ('todo 1');
+INSERT INTO todos (content) values ('todo 2');
+INSERT INTO todos (content) values ('todo 3');
+INSERT INTO todos (content) values ('todo 4');
